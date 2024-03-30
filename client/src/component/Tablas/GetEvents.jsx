b2acc7b88a8650afe99c03d9055c3e4176fd3174
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-03-30 01:29:38 -0600

    lista de eventos
    creacion de componente tabla y sus estilos

--- client/src/component/Tablas/GetEvents.jsx
+++ client/src/component/Tablas/GetEvents.jsx
@@ -1,46 +1,74 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getEvents } from "../../redux/actions";
-
+import { FaPencil, FaEye, FaEraser } from "react-icons/fa6";
 import styles from "./tablas.module.css";
 
 function GetEvents() {
   const dispatch = useDispatch();
   const eventos = useSelector((state) => state.events);
 
+  const handleEdit = (index) => {
+    // Lógica para editar el elemento con el índice proporcionado
+    console.log("Edit item at index:", index);
+  };
+
+  const handleViewDetails = (index) => {
+    // Lógica para ver más detalles del elemento con el índice proporcionado
+    console.log("View details of item at index:", index);
+  };
+
+  const handleDelete = (index) => {
+    // Lógica para eliminar el elemento con el índice proporcionado
+    console.log("Delete item at index:", index);
+  };
   console.log(eventos);
   useEffect(() => {
     dispatch(getEvents());
   }, [dispatch]);
   return (
-    <main className={styles.container}>
-      <h2>Lista de Eventos</h2>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th scope="col">Nombre</th>
+    <main>
+      <h2 className={styles.subtitle}>Lista de Eventos</h2>
+      <div className={styles.container}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
 
-            <th scope="col">Tipo de evento</th>
-            <th scope="col">Fecha de inicio</th>
-            <th scope="col">Fecha de finalizacion</th>
+              <th scope="col">Tipo de evento</th>
+              <th scope="col">Fecha de inicio</th>
+              <th scope="col">Fecha de finalizacion</th>
 
-            <th scope="col">Estado del evento</th>
-          </tr>
-        </thead>
-        <tbody>
-          {eventos.map((evento, index) => (
-            <tr key={index}>
-              <td>{evento.event_name}</td>
-              <td>{evento.event_type}</td>
-
-              <td>{evento.start_date}</td>
-              <td>{evento.end_date}</td>
-
-              <td>{evento.event_state}</td>
+              <th scope="col">Estado del evento</th>
+              <th scope="col">Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {eventos.map((evento, index) => (
+              <tr key={index}>
+                <td>{evento.event_name}</td>
+                <td>{evento.event_type}</td>
+
+                <td>{evento.start_date}</td>
+                <td>{evento.end_date}</td>
+
+                <td>{evento.event_state}</td>
+                <td className={styles.actions}>
+                  <button onClick={() => handleEdit(index)}>
+                    <FaPencil className={styles.icon_mobile} />
+                  </button>
+                  <button onClick={() => handleViewDetails(index)}>
+                    <FaEye className={styles.icon_mobile} />
+                  </button>
+                  <button onClick={() => handleDelete(index)}>
+                    <FaEraser className={styles.icon_mobile} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
